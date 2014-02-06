485ffcb5aa3c6328e3d8b4f61169993f9765117c
Jeremy Green <jeremy@octolabs.com>
2014-02-06 01:30:32 -0600

    Dont activate simplecove and coveralls

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,16 +1,17 @@
 ENV["TEST"] = "true"
 ENV["AUTOLOAD_SPROCKETS"] = "false"
 
-require 'simplecov'
-SimpleCov.start
+#require 'simplecov'
+#SimpleCov.start
 
-require 'coveralls'
-Coveralls.wear!
 
 PROJECT_ROOT_PATH = File.dirname(File.dirname(File.dirname(__FILE__)))
 require "middleman-core"
 require "middleman-core/step_definitions"
 require "middleman-sprockets"
 require File.join(PROJECT_ROOT_PATH, 'lib', 'middleman-automatic-clowncar')
+
+require 'coveralls'
+Coveralls.wear!
 
 
