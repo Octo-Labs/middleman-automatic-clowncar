6b2e57805b9176bf8b269393afc29c5c8038e48d
Jeremy Green <jeremy@octolabs.com>
2014-02-06 01:17:51 -0600

    Add coveralls

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in middleman-automatic-clowncar.gemspec
 gemspec
+
+gem 'coveralls', require: false
--- features/support/env.rb
+++ features/support/env.rb
@@ -4,8 +4,13 @@ ENV["AUTOLOAD_SPROCKETS"] = "false"
 require 'simplecov'
 SimpleCov.start
 
+require 'coveralls'
+Coveralls.wear!
+
 PROJECT_ROOT_PATH = File.dirname(File.dirname(File.dirname(__FILE__)))
 require "middleman-core"
 require "middleman-core/step_definitions"
 require "middleman-sprockets"
 require File.join(PROJECT_ROOT_PATH, 'lib', 'middleman-automatic-clowncar')
+
+
